b93bff5e42a60be4b48538d27bc8b96d28536e90
Szelmaa <katarzyna.zelenka@gmail.com>
2024-02-08 14:51:26 +0100

    moving right/left

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -15,9 +15,12 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 
 # Game loop
 running = True
@@ -29,5 +32,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # if keystroke is pressed check whether is left or right
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
