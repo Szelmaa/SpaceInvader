73310509d38efee8d128e5988a8adfcf55c731dd
Szelmaa <katarzyna.zelenka@gmail.com>
2024-02-11 20:26:22 +0100

    Game Over

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -61,10 +61,16 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
+# Game Over text
+over_font = pygame.font.Font('freesansbold.ttf', 64)
 
 def show_score(x, y):
     score = font.render('Score: ' + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render('GAME OVER', True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -127,6 +133,13 @@ while running:
 
     # enemy movement
     for i in range(num_of_enemies):
+        # Game Over
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
             enemyX_change[i] = 0.1
@@ -139,7 +152,7 @@ while running:
         # Collision, new enemy respawn
         collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
         if collision:
-            explosion_Sound = mixer.Sound('explosion.wav.wav')
+            explosion_Sound = mixer.Sound('explosion.wav')
             explosion_Sound.play()
             bulletY = 480
             bullet_state = 'ready'
